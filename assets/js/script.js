4e11507671787ac05bc152ea45869c42778ff2e5
Wajd Alasttal <wajd.dev@gmail.com>
2023-08-03 15:08:44 +0300

    added-slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,27 @@ $(document).ready(function(){
 }
 );
 });
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
