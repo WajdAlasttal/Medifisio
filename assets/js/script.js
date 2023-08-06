978dd7a3a1565004b158c92494fab9e4dcb03092
Wajd Alasttal <wajd.dev@gmail.com>
2023-08-06 23:33:27 +0300

    fixed-responsive-navbar-slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,41 @@
-$(document).ready(function(){
-    $('.fa-bars').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
+function openNavbar() {
+  document.getElementById("sidenav").style.display = "block";
+  document.getElementById("sidenav").style.width = "50%";
 }
-);
-});
+function closeNavbar() {
+  document.getElementById("sidenav").style.width = "0%";
+}
 
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 3,
+  spaceBetween: 20,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("rtl-ltr-toggle");
+
+  toggleButton.addEventListener("click", function () {
+    const currentDir = document.body.getAttribute("dir");
+    const newDir = currentDir === "rtl" ? "ltr" : "rtl";
+    document.body.setAttribute("dir", newDir);
   });
-
-  document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.getElementById("rtl-ltr-toggle");
-
-    toggleButton.addEventListener("click", function() {
-        const currentDir = document.body.getAttribute("dir");
-        const newDir = currentDir === "rtl" ? "ltr" : "rtl";
-        document.body.setAttribute("dir", newDir);
-    });
 });
